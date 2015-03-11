be4c04c8d85697fa8712a12d011fe03f1954df9f
cpn <cpn@cpn.cpn>
2015-03-11 18:17:22 +0300

    3 и 4 задачи

--- section_2.py
+++ section_2.py
@@ -55,16 +55,46 @@ def addition_usual(number):
     return add
 
 
-def addition_range():
+def addition_range(start=0, count=5):
     """
     Написать фабрику, аналогичную п.2, но возвращающей список таких функций
 
     # список из функций сложения от 0 до 5 включительно
+    # т.е. аналогичное [add0, add1, add2, add3, add4, add5]
     >>> additionals = addition_range(0, 5)
-
-    т.е. аналогичное [add0, add1, add2, add3, add4, add5]
+    >>> res = []
+    >>> for i in additionals:
+    ...     res.append(i(2))
+    >>> res
+    [2, 3, 4, 5, 6, 7]
     """
-    pass
+    res = []
+    for i in xrange(start, count+1):
+        res.append(lambda x, i=i: x+i)
+    return res
+
+
+def mymap(*args):
+    """
+    Написать аналог map:
+
+    первым аргументом идет либо функция, либо список функций
+    вторым аргументом — список аргументов, которые будут переданы функциям
+    полагается, что эти функции — функции одного аргумента
+    в данном случае "развернутая" запись будет:
+    [
+        (add0(1), add0(2), add0(3)),
+        (add1(1), add1(2), add1(3)),
+        (add2(1), add2(2), add2(3))
+    ]
+    >>> mymap(addition_range(0, 2), [1, 2, 3])
+    [(1, 2, 3), (2, 3, 4), (3, 4, 5)]
+    """
+    func, lst = args
+    if isinstance(func, list):
+        return [tuple(map(i, lst)) for i in func]
+    else:
+        return tuple(map(func, lst))
 
 
 if __name__ == "__main__":
